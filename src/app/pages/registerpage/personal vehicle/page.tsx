2507bbf5288c9625ce047eb794155cfe771cd436
Valenti25 <mak0623077175@gmail.com>
2025-10-17 23:11:27 +0700

    update ui fontend registerpage

---
+++ src/app/pages/registerpage/personal vehicle/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from '@/app/components/navbar/page';
+// import { InputGroup, InputGroupAddon, InputGroupInput } from '@/components/ui/input-group';
+// import { Button } from '@/components/ui/button';
+// import Link from 'next/link';
+export default function RegisterServicePage() {
+    return (
+        <div className="container">
+            <Navbar />
+        </div>
+    );
+}
